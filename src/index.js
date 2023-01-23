51bba1fcf1415e01a11d59896f424967a4c219b5
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-01-23 20:06:21 +0530

    build added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import App from './App';
 
 
@@ -10,4 +10,3 @@ root.render(
     <App />
   </>
 );
-serviceWorkerRegistration.register();
