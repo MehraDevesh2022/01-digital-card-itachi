a55b0ce1e2b7238672ba99174a96a2325ca4b039
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-01-23 20:20:54 +0530

    maskable update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import App from './App';
-
 
 ReactDOM.render(
   <React.StrictMode>
